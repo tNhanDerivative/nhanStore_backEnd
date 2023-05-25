fbeaeaac70bd8dedf485925ba448688d17d959a2
trongnhan <tnhanderivative@zohomail.com>
2023-05-26 00:53:34 +0700

    rename file, product model fix price type

--- SammyStore/product/models.py
+++ SammyStore/product/models.py
@@ -29,7 +29,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField()
     description = models.TextField(blank=True, null=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=1)
     image = models.ImageField(upload_to='uploads/', blank=True, null=True)
     thumbnail = models.ImageField(upload_to='uploads/', blank=True, null=True)
     date_added = models.DateTimeField(auto_now_add=True)
