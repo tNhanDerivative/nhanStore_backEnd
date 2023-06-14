20022df200d37ddb0c1cbcbe59c38a7730ad32ed
trongnhan <tnhanderivative@zohomail.com>
2023-06-14 13:53:36 +0700

    fix setting files

--- SammyStore/SammyStore/settings/dev.py
+++ SammyStore/SammyStore/settings/dev.py
@@ -3,7 +3,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-ALLOWED_HOSTS = ['127.0.0.1']
+DEBUG = True
+ALLOWED_HOSTS = ['127.0.0.1',os.getenv('SERVER_IP')]
 
 CORS_ALLOWED_ORIGINS = [
     os.getenv('CORS_ALLOWED_ORIGINS'),
--- SammyStore/SammyStore/settings/prod.py
+++ SammyStore/SammyStore/settings/prod.py
@@ -4,7 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-ALLOWED_HOSTS = ['127.0.0.1',os.getenv('SERVER_IP'),  os.getenv('DOMAIN_NAME')]
+DEBUG = True
+ALLOWED_HOSTS = ['127.0.0.1','localhost',os.getenv('SERVER_IP'),  os.getenv('DOMAIN_NAME')]
 
 CORS_ALLOWED_ORIGINS = [
     os.getenv('CORS_ALLOWED_ORIGINS'),
