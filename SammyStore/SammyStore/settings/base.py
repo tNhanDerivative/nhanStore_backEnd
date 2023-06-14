9004034f65c0fd1b1979035873b0bb6eeafa5bf7
trongnhan <tnhanderivative@zohomail.com>
2023-06-14 13:56:12 +0700

    fix setting files again

--- SammyStore/SammyStore/settings/base.py
+++ SammyStore/SammyStore/settings/base.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
 CORS_ALLOW_HEADERS = (
     'accept',
     'accept-encoding',
